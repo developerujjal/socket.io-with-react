603d504965a8b6c9f92457f73623b8f7665f71a3
developerujjal <ujjal.vision02@gmail.com>
2025-03-31 00:09:32 +0600

    full setup done

--- chat-room-app-backend/index.js
+++ chat-room-app-backend/index.js
@@ -1,12 +1,44 @@
 const express = require('express');
 const app = express();
+const cors = require('cors');
 const { createServer } = require('http');
 const { Server } = require('socket.io');
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5000;
+
+const allowOrigin = [
+    'http://localhost:5173'
+]
+const corsOptions = {
+    origin: allowOrigin, // Your React client's URL
+    methods: ["GET", "POST", "PUT", "PATCH", "DELETE"], // Best for REST APIs
+    optionsSuccessStatus: 200, // For legacy browser support
+    // credentials: true
+};
 
 const expressServer = createServer(app);
 
-const io = new Server(expressServer);
+const io = new Server(expressServer, {
+    cors: {
+        origin: allowOrigin, // Your frontend URL
+        methods: ["GET", "POST"], // Only these are needed
+        // credentials: true // If using cookies/auth
+    }
+})
+
+
+app.use(express.json())
+app.use(cors(corsOptions));
+
+io.on('connection', (socket) => {
+    console.log('new user join.')
+
+    
+    socket.on('disconnect', () => {
+        console.log('user disconnected')
+    })
+})
+
+
 
 
 expressServer.listen(port, () => {
--- chat-room-app/src/App.jsx
+++ chat-room-app/src/App.jsx
@@ -3,7 +3,7 @@ import { io } from "socket.io-client";
 
 function App() {
   useEffect(() => {
-    const socket = io('http://localhost:3000')
+    const socket = io('http://localhost:5000')
   }, []);
 
   return (
